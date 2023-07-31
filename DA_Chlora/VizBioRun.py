fdfb9efab0612e78c6e3193c88ba6c2c3b3abce8
Olmo Zavala <olmozavala@gmail.com>
2023-07-31 12:02:47 -0400

    UPdating main first viz

--- DA_Chlora/VizBioRun.py
+++ DA_Chlora/VizBioRun.py
@@ -4,10 +4,11 @@ sys.path.append("eoas_pyutils")
 
 from viz_utils.eoa_viz import EOAImageVisualizer
 from viz_utils.constants import PlotMode, BackgroundType
-from proc_utils.gom import lc_from_date 
-from io_utils.coaps_io_data import get_aviso_by_month, get_aviso_by_date, get_chlora_noaa_by_date, get_hycom_gom_raw_by_date, get_sst_by_date
+from proc_utils.gom import lc_from_date, lc_from_ssh
+from io_utils.coaps_io_data import get_aviso_by_month, get_aviso_by_date, get_biorun_cicese_nemo_by_date, get_chlora_noaa_by_date, get_hycom_gom_raw_by_date, get_sst_by_date
 
 from shapely.geometry import LineString
+from matplotlib.colors import LogNorm
 import xarray as xr
 import cmocean.cm as ccm
 import os
@@ -17,8 +18,8 @@ import numpy as np
 
 # %% 
 # Generate an array of datetimes from 2010 to 2020
-start_date = datetime(2018, 3, 5)
-end_date = datetime(2018, 3, 7)
+start_date = date(2019, 3, 5)
+end_date = date(2019, 3, 7)
 dates = [start_date + timedelta(days=x) for x in range(0, (end_date - start_date).days)]
 
 aviso_folder = "/unity/f1/ozavala/DATA/GOFFISH/AVISO/GoM/"
@@ -48,25 +49,27 @@ chlora_data, _,  _= get_chlora_noaa_by_date(chlora_folder, c_date, bbox=bbox)
 
 
 #  %%-------- Read HYCOM data
-# hycom_data, _,  _=  get_hycom_gom_raw_by_date(c_date, bbox)
+hycom_data, _,  _=  get_hycom_gom_raw_by_date(c_date, bbox)
 
 # # Select current day
-# hycom_today_ssh = hycom_data.ssh[0, :, :]
-# hycom_today_sst = hycom_data.temperature[0, 0, :, :]
+hycom_today_ssh = hycom_data.ssh[0, :, :]
+hycom_today_sst = hycom_data.temperature[0, 0, :, :]
 
 # %% -------- Read Stellite SST data
 sst_data, lats_sst, lons_sst= get_sst_by_date(satellite_sst_folder, c_date, bbox=bbox)
 
+# %% -------- Biorun from cicese
+biorun, lats, lons = get_biorun_cicese_nemo_by_date(c_date, bbox)
+
 # %%--------------- Interpolate all the data to the lat and lon from sst satellite
+biorun = biorun.interp(latitude=lats_sst, longitude=lons_sst)
 aviso_today = aviso_today.interp(latitude=lats_sst, longitude=lons_sst)
 chlora_data =  chlora_data.interp(latitude=lats_sst, longitude=lons_sst)
-# hycom_today_ssh = hycom_today_ssh.interp(latitude=lats_sst, longitude=lons_sst)
-# hycom_today_sst = hycom_today_sst.interp(latitude=lats_sst, longitude=lons_sst)
+hycom_today_ssh = hycom_today_ssh.interp(latitude=lats_sst, longitude=lons_sst)
+hycom_today_sst = hycom_today_sst.interp(latitude=lats_sst, longitude=lons_sst)
 
 
 # %% --------------- Plot all the obtained datasets -----------------------
-# Visualize the data in a 3 by 2 grid. Using cmocean colormap for each variable
-from matplotlib.colors import LogNorm
 viz_obj = EOAImageVisualizer(lats=lats_sst, lons=lons_sst, disp_images=True, 
                             max_imgs_per_row=2, fig_size=15,
                              output_folder="imgs", show_var_names=True)
@@ -74,7 +77,23 @@ viz_obj = EOAImageVisualizer(lats=lats_sst, lons=lons_sst, disp_images=True,
 lc = lc_from_date(c_date)
 mylinestring = LineString(list(lc))
 viz_obj.__setattr__('additional_polygons', [mylinestring])
-viz_obj.plot_2d_data_np(np.array([aviso_today, aviso_today, aviso_today, sst_data['analysed_sst'][0, :, :], chlora_data['chlor_a'][0, 0, :, :]]), 
-# viz_obj.plot_2d_data_np(np.array([aviso_today, hycom_today_ssh, hycom_today_sst, sst_data['analysed_sst'][0, :, :], chlora_data['chlor_a'][0, 0, :, :]]), 
+viz_obj.plot_2d_data_np(np.array([aviso_today, hycom_today_ssh, hycom_today_sst, 
+                                  sst_data['analysed_sst'][0, :, :], chlora_data['chlor_a'][0, 0, :, :]]), 
                         ['AVISO ADT', 'HYCOM SSH', 'HYCOM SST', 'Satellite SST', 'Chlor-a NOAA'],
-                        norm = np.array([None, None, None, None, LogNorm()]), file_name_prefix=c_date_str, title=c_date_str)
+                        norm = np.array([None, None, None, None, LogNorm(0.07,1)]), 
+                        file_name_prefix=c_date_str, title=c_date_str)
+
+
+# %%
+viz_obj = EOAImageVisualizer(lats=lats_sst, lons=lons_sst, disp_images=True, 
+                            max_imgs_per_row=2, fig_size=15,
+                             output_folder="imgs", show_var_names=True)
+
+lc_bio = lc_from_ssh(biorun.ssh[0, :, :].values, lons_sst, lats_sst, np.nanmean(biorun.ssh[0, :, :].values))
+mylinestring = LineString(list(lc_bio))
+viz_obj.__setattr__('additional_polygons', [mylinestring])
+viz_obj.plot_2d_data_np(np.array([biorun.nchl[0, :, :], biorun.dchl[0, :, :], biorun.ssh[0, :, :], biorun.temperature[0, :, :]]), 
+                        ['Biorun NCHL', 'Biorun DCHL', 'Biorun SSH', 'Biorun SST'],
+                        norm = np.array([LogNorm(.03,.30), LogNorm(0.005,.3), None, None]), 
+                        file_name_prefix=c_date_str, title=c_date_str)
+# %%
