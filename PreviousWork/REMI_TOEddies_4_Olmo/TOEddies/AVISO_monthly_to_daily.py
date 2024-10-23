589b3a3d87f0d950fd7e39a3bfec9335bf827cf7
Jorge Velasco Zavala <jv24b@fsu.edu>
2024-10-23 12:44:04 -0400

    Updating the trainning modes

--- PreviousWork/REMI_TOEddies_4_Olmo/TOEddies/AVISO_monthly_to_daily.py
+++
@@ -1,9 +0,0 @@
-import xarray as xr
-from os.path import join
-import os
-
-
-in_folder = "/data/GOFFISH/AVISO"
-out_folder = "/data/GOFFISH/AVISO_daily"
-
-files = os.listdir(in_folder)
--- README.md
+++
@@ -1,2 +0,0 @@
-# ugos3_task21
-Code relevant to task 2.1 of the UGOS 3 project
